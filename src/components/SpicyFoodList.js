ad3f70b02e7ad286cc0a084f4d08b0be1854700b
shirocodes <ishirapt@gmail.com>
2025-04-12 18:17:25 +0300

    Completed the lab

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -4,13 +4,23 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
 
+  console.log("the foods are:", foods)
+
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    console.log("newfoods are:", newFood);
+
+    const newFoodArray = [...foods, newFood]
+    setFoods(newFoodArray)
+  }
+
+  function handleLiClick(id) {
+    const newFoodArray = foods.filter((food) => food.id !== id)
+    setFoods(newFoodArray)
   }
 
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
